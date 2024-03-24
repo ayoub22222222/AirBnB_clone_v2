28e25c5662b8421164320e6d07acbc69d01161a6
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-03-24 00:52:16 +0000

    l

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,12 @@
--- prepares a MySQL server for the project
+-- first 3 students in the Batch ID=3
+-- because Batch 3 is the best!
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+SET FOREIGN_KEY_CHECKS=1;
 
