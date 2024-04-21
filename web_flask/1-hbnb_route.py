e26084f8ea2b7b5e18ba8e713e59f41fbee2cf2c
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-04-21 13:12:43 +0000

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+description about this folder
+start a flask application with ip 0.0.0.0
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """ display hello hbnb """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_route():
+    """ display hbnb """
+    return "HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
