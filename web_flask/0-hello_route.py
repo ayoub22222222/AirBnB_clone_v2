3c6ec093f797e00f862b872ee2233952d5656b14
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-04-21 13:06:05 +0000

    task 0 fix 001

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 description about this folder
 start a flask application with ip 0.0.0.0
 """
@@ -8,10 +8,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_route():
     """ display hbnb """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-	app.run(host= "0.0.0.0")
+    app.run(host="0.0.0.0")
